d0c8ae17bd91a707ab93a1d3e5288b6d4e943fcd
Omnivium <omnivium@users.noreply.github.com>
2026-05-23 04:20:34 +0800

    修复了一些问题

--- mobile/android/settings.gradle.kts
+++ mobile/android/settings.gradle.kts
@@ -12,11 +12,11 @@ pluginManagement {
 
     repositories {
         gradlePluginPortal()
+        google()
+        mavenCentral()
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        google()
-        mavenCentral()
     }
 }
 
